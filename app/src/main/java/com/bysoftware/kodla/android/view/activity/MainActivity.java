b688844abf734e7be6ea607036e402ee85800724
Satuk Buğrahan Öztürk <sbozturk1907@gmail.com>
2017-12-24 19:24:11 +0300

    share button func. added

--- app/src/main/java/com/bysoftware/kodla/android/view/activity/MainActivity.java
+++ app/src/main/java/com/bysoftware/kodla/android/view/activity/MainActivity.java
@@ -2,11 +2,13 @@ package com.bysoftware.kodla.android.view.activity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bysoftware.kodla.android.R;
@@ -68,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
         return new SimpleCircleButton.Builder().listener(new OnBMClickListener() {
             @Override
             public void onBoomButtonClick(int index) {
-                int i = (int)(Math.random()*4+1);
+                int i = (int) (Math.random() * 4 + 1);
                 if (index == 0) {
                     createDialog("github", i);
                     Toast.makeText(MainActivity.this, "GitHub", Toast.LENGTH_SHORT).show();
@@ -93,76 +95,107 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createDialog(String lesson, int tip) {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        String message = null;
         if (tip == 1) {
             if (lesson.equalsIgnoreCase("github")) {
                 alertDialogBuilder.setMessage(R.string.github_tip_1);
+                message = getString(R.string.github_tip_1);
             } else if (lesson.equalsIgnoreCase("java")) {
                 alertDialogBuilder.setMessage(R.string.java_tip_1);
+                message = getString(R.string.java_tip_1);
             } else if (lesson.equalsIgnoreCase("js")) {
                 alertDialogBuilder.setMessage(R.string.js_tip_1);
+                message = getString(R.string.js_tip_1);
             } else if (lesson.equalsIgnoreCase("kotlin")) {
                 alertDialogBuilder.setMessage(R.string.kotlin_tip_1);
+                message = getString(R.string.kotlin_tip_1);
             } else if (lesson.equalsIgnoreCase("swift")) {
                 alertDialogBuilder.setMessage(R.string.swift_tip_1);
+                message = getString(R.string.swift_tip_1);
             } else if (lesson.equalsIgnoreCase("android")) {
                 alertDialogBuilder.setMessage(R.string.android_tip_1);
+                message = getString(R.string.android_tip_1);
             }
         } else if (tip == 2) {
             if (lesson.equalsIgnoreCase("github")) {
                 alertDialogBuilder.setMessage(R.string.github_tip_2);
+                message = getString(R.string.github_tip_2);
             } else if (lesson.equalsIgnoreCase("java")) {
                 alertDialogBuilder.setMessage(R.string.java_tip_2);
+                message = getString(R.string.java_tip_2);
             } else if (lesson.equalsIgnoreCase("js")) {
                 alertDialogBuilder.setMessage(R.string.js_tip_2);
+                message = getString(R.string.js_tip_2);
             } else if (lesson.equalsIgnoreCase("kotlin")) {
                 alertDialogBuilder.setMessage(R.string.kotlin_tip_2);
+                message = getString(R.string.kotlin_tip_2);
             } else if (lesson.equalsIgnoreCase("swift")) {
                 alertDialogBuilder.setMessage(R.string.swift_tip_2);
+                message = getString(R.string.swift_tip_2);
             } else if (lesson.equalsIgnoreCase("android")) {
                 alertDialogBuilder.setMessage(R.string.android_tip_2);
+                message = getString(R.string.android_tip_2);
             }
         } else if (tip == 3) {
             if (lesson.equalsIgnoreCase("github")) {
                 alertDialogBuilder.setMessage(R.string.github_tip_3);
+                message = getString(R.string.github_tip_3);
             } else if (lesson.equalsIgnoreCase("java")) {
                 alertDialogBuilder.setMessage(R.string.java_tip_3);
+                message = getString(R.string.java_tip_3);
             } else if (lesson.equalsIgnoreCase("js")) {
                 alertDialogBuilder.setMessage(R.string.js_tip_3);
+                message = getString(R.string.js_tip_3);
             } else if (lesson.equalsIgnoreCase("kotlin")) {
                 alertDialogBuilder.setMessage(R.string.kotlin_tip_3);
+                message = getString(R.string.kotlin_tip_3);
             } else if (lesson.equalsIgnoreCase("swift")) {
                 alertDialogBuilder.setMessage(R.string.swift_tip_3);
+                message = getString(R.string.swift_tip_3);
             } else if (lesson.equalsIgnoreCase("android")) {
                 alertDialogBuilder.setMessage(R.string.android_tip_3);
+                message = getString(R.string.android_tip_3);
             }
         } else if (tip == 4) {
             if (lesson.equalsIgnoreCase("github")) {
                 alertDialogBuilder.setMessage(R.string.github_tip_4);
+                message = getString(R.string.github_tip_4);
             } else if (lesson.equalsIgnoreCase("java")) {
                 alertDialogBuilder.setMessage(R.string.java_tip_4);
+                message = getString(R.string.java_tip_4);
             } else if (lesson.equalsIgnoreCase("js")) {
                 alertDialogBuilder.setMessage(R.string.js_tip_4);
+                message = getString(R.string.js_tip_4);
             } else if (lesson.equalsIgnoreCase("kotlin")) {
                 alertDialogBuilder.setMessage(R.string.kotlin_tip_4);
+                message = getString(R.string.kotlin_tip_4);
             } else if (lesson.equalsIgnoreCase("swift")) {
                 alertDialogBuilder.setMessage(R.string.swift_tip_4);
+                message = getString(R.string.swift_tip_4);
             } else if (lesson.equalsIgnoreCase("android")) {
                 alertDialogBuilder.setMessage(R.string.android_tip_4);
+                message = getString(R.string.android_tip_4);
             }
         } else if (tip == 5) {
             if (lesson.equalsIgnoreCase("github")) {
                 alertDialogBuilder.setMessage(R.string.github_tip_5);
+                message = getString(R.string.github_tip_5);
             } else if (lesson.equalsIgnoreCase("java")) {
                 alertDialogBuilder.setMessage(R.string.java_tip_5);
+                message = getString(R.string.java_tip_5);
             } else if (lesson.equalsIgnoreCase("js")) {
                 alertDialogBuilder.setMessage(R.string.js_tip_5);
+                message = getString(R.string.js_tip_5);
             } else if (lesson.equalsIgnoreCase("kotlin")) {
                 alertDialogBuilder.setMessage(R.string.kotlin_tip_5);
+                message = getString(R.string.kotlin_tip_5);
             } else if (lesson.equalsIgnoreCase("swift")) {
                 alertDialogBuilder.setMessage(R.string.swift_tip_5);
+                message = getString(R.string.swift_tip_5);
             } else if (lesson.equalsIgnoreCase("android")) {
                 alertDialogBuilder.setMessage(R.string.android_tip_5);
+                message = getString(R.string.android_tip_5);
             }
         }
         alertDialogBuilder.setPositiveButton("OK",
@@ -171,12 +204,18 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface arg0, int arg1) {
                     }
                 });
+        final String mes = message;
 
         alertDialogBuilder.setNegativeButton("SHARE",
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        //TODO: Add share function
+                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "New Information About Coding");
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, mes);
+                        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
                     }
                 });
 
